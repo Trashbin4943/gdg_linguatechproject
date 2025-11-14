623a0a2237579e5c50c25d5053e5c93c2a640683
Trashbin4943 <anthonyhwan415@gmail.com>
2025-11-14 19:03:42 +0900

    FIX: 가상환경 수정

---
+++ vevn_3.11/Scripts/django-startproject.py
@@ -0,0 +1,9 @@
+#!C:\Dev\LinguaTech\linguaproject\vevn_3.11\Scripts\python.exe
+
+def main():
+    from django_startproject.management import start_project
+    start_project()
+
+
+if __name__ == '__main__':
+    main()
